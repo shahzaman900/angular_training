5725ae7376669328f384bbc70302a8368e1eac6f
sheezu900 <shahzamanlive2@gmail.com>
2023-11-06 18:23:38 +0500

    change color on click

--- src/app/sorting/sorting.component.ts
+++ src/app/sorting/sorting.component.ts
@@ -7,10 +7,28 @@ import { Component } from '@angular/core';
 })
 export class SortingComponent {
 
-  buttonColor: string = 'blue'
 
-  sorting() {
-    this.buttonColor = "red"
+  colors: string[] = ['red', 'green', 'blue'];
+  currentColorIndex: number = 0;
+  buttonColor: string = this.colors[this.currentColorIndex];
+
+  changeButtonColor() {
+    this.currentColorIndex = (this.currentColorIndex + 1) % this.colors.length;
+    this.buttonColor = this.colors[this.currentColorIndex];
   }
+
+  // buttonColor: string = 'white'
+  // status: number = 1
+
+  // sorting() {
+  //   // if (this.status === 1) {
+  //   //   this.buttonColor = 'blue'
+  //   // } if (this.status === 2) {
+  //   //   this.buttonColor = 'green'
+  //   // } else {
+  //   //   this.buttonColor
+  //   // }
+  //   // this.status++
+  // }
 
 }
